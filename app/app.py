1bbaef5d4984000df979fe55794ef743f57b47a4
Fawad <fawadahmed322@gmail.com>
2024-09-14 05:34:08 +0200

    Fixed Styling Issues

--- app/app.py
+++ app/app.py
@@ -1,14 +1,14 @@
-# app/app.py
-
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 todos = []
 
+
 @app.route('/todos', methods=['GET'])
 def get_todos():
     return jsonify(todos), 200
+
 
 @app.route('/todos', methods=['POST'])
 def add_todo():
@@ -16,5 +16,8 @@ def add_todo():
     todos.append(todo)
     return jsonify({'message': 'Todo added!'}), 201
 
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
+
+# Ensure the file ends with a blank line
